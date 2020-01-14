0cb926f415edbe3e545a82ed6188d005f05a779d
edison.vasquez@sofka.com.co <edison.vasquez@sofka.com.co>
2020-01-14 11:01:42 -0500

    se cambia port para desplegar en jenkinss

--- src/app.js
+++ src/app.js
@@ -28,5 +28,5 @@ app.use('/', qualityRoutes);
 app.use(express.static(path.join(__dirname,'public')));
 
 app.listen(app.get('port'), () => {
-    console.log('Server on port 3000');
+    console.log('Server on port 3001');
 });
